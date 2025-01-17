c06ab6cf1ed6c7684f65e8d0365c943ca32a8b40
MEENAKSHI2113007 <151904758+MEENAKSHI2113007@users.noreply.github.com>
2025-01-17 22:44:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const pokemonRoutes = require('./routes/pokemonRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const MONGO_URI = 'mongodb://localhost:27017/pokemon';
+const PORT = 5000;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+app.use('/api/pokemons', pokemonRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
