21a9b97267f4da93146abb9328d1c303e76244f9
MEENAKSHI2113007 <151904758+MEENAKSHI2113007@users.noreply.github.com>
2025-01-17 22:45:18 +0530

    Update Pokemon.js

--- backend/models/Pokemon.js
+++ backend/models/Pokemon.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const pokemonSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  height: { type: Number },
+  weight: { type: Number },
+  image: { type: String },
+});
+
+module.exports = mongoose.model('Pokemon', pokemonSchema);
 
