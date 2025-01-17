06d13808e686db49d7b0c7acc5ed721074375ae7
MEENAKSHI2113007 <151904758+MEENAKSHI2113007@users.noreply.github.com>
2025-01-17 22:44:49 +0530

    Update seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -1,1 +1,42 @@
+const mongoose = require('mongoose');
+const axios = require('axios');
+const Pokemon = require('./models/Pokemon');
+
+const MONGO_URI = 'mongodb://localhost:27017/pokemon';
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected for seeding'))
+  .catch((err) => console.error(err));
+
+const seedPokemon = async () => {
+  try {
+    await Pokemon.deleteMany();
+
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=50');
+    const results = response.data.results;
+
+    const pokemons = [];
+    for (const result of results) {
+      const details = await axios.get(result.url);
+      pokemons.push({
+        id: details.data.id,
+        name: details.data.name,
+        type: details.data.types.map((t) => t.type.name).join(', '),
+        height: details.data.height,
+        weight: details.data.weight,
+        image: details.data.sprites.front_default,
+      });
+    }
+
+    await Pokemon.insertMany(pokemons);
+    console.log('Database seeded successfully');
+    mongoose.connection.close();
+  } catch (err) {
+    console.error(err);
+    mongoose.connection.close();
+  }
+};
+
+seedPokemon();
 
