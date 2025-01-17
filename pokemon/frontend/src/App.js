de207b2f447a156fd0dbc29fbdab023cd947fedc
MEENAKSHI2113007 <151904758+MEENAKSHI2113007@users.noreply.github.com>
2025-01-17 22:57:32 +0530

    Update App.js

--- pokemon/frontend/src/App.js
+++ pokemon/frontend/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import PokemonList from './components/PokemonList';
+import PokemonDetails from './components/PokemonDetails';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:id" element={<PokemonDetails />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
 
